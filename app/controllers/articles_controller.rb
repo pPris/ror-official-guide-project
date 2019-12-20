cac3e0ddd8b652dc34b044a7d02932cb1f7d3cad
pris <49683462+pPris@users.noreply.github.com>
2019-12-20 12:11:41 +0800

    add update articles functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,28 +4,42 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
+       
+    def edit
+        @article = Article.find(params[:id])
+      end
+
+    def create
         # security risks here - need to use "strong parameters"
         # @article = Article.new(params[:article])
  
         # @article.save
         # redirect_to @article
 
-    def new
-        @article = Article.new
-      end
-       
-      def create
         @article = Article.new(article_params)
-       
+        
         if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
           redirect_to @article
         else
-          render 'new'
+          render 'edit'
         end
-      end
-       
-      private
-        def article_params
-          params.require(:article).permit(:title, :text)
-        end
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
