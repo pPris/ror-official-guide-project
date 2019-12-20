a975f10705d8b6fafd206c72e5b847b9b41f187f
pris <49683462+pPris@users.noreply.github.com>
2019-12-20 14:45:24 +0800

    set up new model and related actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    def index # this action might have been needed for 5.11 to work
+        @articles = Article.all
+    end
 
     def show
         @article = Article.find(params[:id])
@@ -38,6 +41,13 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+       
+        redirect_to articles_path
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :text)
